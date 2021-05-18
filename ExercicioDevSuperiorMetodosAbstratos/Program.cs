ea49e82e9c5dccafb52c107290382246f76ad2fa
gunn3r71 <lucasgunner3@gmail.com>
2021-05-17 22:16:25 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDevSuperiorMetodosAbstratos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioDevSuperiorMetodosAbstratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
